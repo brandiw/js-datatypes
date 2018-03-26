618e8ff4876d26edaa09841ae7803e0537f63d7a
jessiheger <jhmheger@gmail.com>
2018-03-26 16:10:43 -0700

    1st commit - selecting datatypes

---
+++ w2d1_answers.js
@@ -0,0 +1,24 @@
+// https://git.generalassemb.ly/wdi-wc-march2018/Homework/blob/master/Week_Two/Monday/JS_Datatypes.md
+
+// Datatypes
+// Answer the Following
+// For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
+
+// 1. A light switch that can be either on or off.
+// boolean
+
+// 2. A user's email address.
+// string
+
+// 3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// object
+
+// 4. A list of student names from our class.
+// array
+
+// 5. A list of student names from our class, each with a location.
+// object
+
+// 6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// object
+
