97fdb45db1965a8ae45cc297367adfb715548f39
simvirk5 <simvirk5@gmail.com>
2018-03-26 16:25:36 -0700

    selecting datatypes

---
+++ answers.js
@@ -0,0 +1,21 @@
+const lightSwitch = {
+	on: 'true',
+	}
+i.e. Boolean
+const userInfo = {
+	name: "x",
+	emailaddress: "johndoe@gmail.com"
+}
+i.e. Boolean, string, object, array
+
+const spaceship = [
+	{hull: "true",
+	laser: "false",
+	tractorbeam: "low",
+	warpdrive: "none",
+} ]
+i.e. string, array, object
+
+const namesOfStudents = {
+	[name: '', location: '', favoriteShow: '']}
+i.e. strings, arrays within an object, use (sort) function
