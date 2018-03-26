9d6a7f43668db0b64285a312a092a04735779920
bradserlis <bradserlis@yahoo.com>
2018-03-26 16:41:03 -0700

    commit 3 - performed operations on the crazy object

--- answers.js
+++ answers.js
@@ -6,9 +6,9 @@
 //6. student names / location/ favorite show = array of objects
 
 //take it easy
-// 1. const rainbow = {'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'}
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-// 2. console.log(rainbow[4]);
+console.log(rainbow[4]);
 
 // const bradley = {
 // 	favoritefood: "pizza",
@@ -19,4 +19,44 @@
 
 // 4. bradley.hobby;
 
-//
+//crazy object
+
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [{
+      name: "Jeff",
+      occupation: "manager"
+    },
+    {
+      name: "funkhauser",
+      occupation: "tv dude"
+    },
+    {
+      name: "susie",
+      occupation: "jeffs wife",
+      favouriteHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
+}
+
+// 1. crazyObject.taco[1].salsa[5];
+// 2. crazyObject.larry.quotes[0];
+// 3. crazyObject.larry.characters[2].favouriteHobby;
+// 4. crazyObject.larry.nicknames[1]
+// 5. crazyObject.larry.characters.forEach((item)=>{
+	// 	if(item.name == "funkhauser"){
+	// 		console.log(item.name)
+	// 	}
+	// })
+ 
+
+
+
