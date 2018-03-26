e5217010db17d3a662f846d3f1142854f06540ca
bradserlis <bradserlis@yahoo.com>
2018-03-26 15:56:28 -0700

    2nd commit - created some data structures and accessed some data

--- answers.js
+++ answers.js
@@ -4,3 +4,19 @@
 //4. student names for a class = array
 //5. student names w/ location = array of objects
 //6. student names / location/ favorite show = array of objects
+
+//take it easy
+// 1. const rainbow = {'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'}
+
+// 2. console.log(rainbow[4]);
+
+// const bradley = {
+// 	favoritefood: "pizza",
+// 	hobby: "music",
+// 	townLivedIn: "Redmond",
+// 	favoriteDatatype: "objects"
+// }
+
+// 4. bradley.hobby;
+
+//
