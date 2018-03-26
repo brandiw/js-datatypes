7e8a9c390d11435bf34c5601c2bb06ca66e5568e
bradserlis <bradserlis@yahoo.com>
2018-03-26 15:53:32 -0700

    1st commit - selecting datatypes

---
+++ answers.js
@@ -0,0 +1,6 @@
+//1. the light switch would be an object, with a boolean for whether the switch is on/off
+//2. email = string
+//3. spaceship = object
+//4. student names for a class = array
+//5. student names w/ location = array of objects
+//6. student names / location/ favorite show = array of objects
