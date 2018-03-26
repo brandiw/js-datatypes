536561f6e0a1f08567503567bdee450cff47ea9b
ortizk <ortizk@live.com>
2018-03-26 16:23:31 -0700

    first commit - Selecting Data Types

---
+++ answers.js
@@ -0,0 +1,15 @@
+// Answer The Following
+// 	1. Boolean ie/ lightBulbOn: true
+// 	2. String ie/ email: "ortizk@live.com"
+// 	3. Array ie/ spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"]
+// 	4. Array ie/ students = ["Karla", "Simmy", "Jesse"]
+// 	5. Object ie/ stuLoc = [
+// 						{name: "Karla", location: "Fist Hill"},
+// 						{name: "Simmy", location: "Bothell"},
+// 						{name: "Jesse", location: "Queen Anne"}
+// 						];
+// 	6.Object ie/ stuLoc = [
+// 						{name: "Karla", location: "Fist Hill", faveTv: "Broad City"},
+// 						{name: "Simmy", location: "Bothell", faveTv: "Walking Dead"},
+// 						{name: "Jesse", location: "Queen Anne", faveTv: "The Bachelor"}
+// 						];
