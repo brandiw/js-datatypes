ed334eca2e52a312ab873142e3c450bc2016eadc
smithalex24 <smith25243@gmail.com>
2018-03-26 19:48:59 -0700

    2nd commit - created some data structures and accessed some data

--- answers.js
+++ answers.js
@@ -1,9 +1,34 @@
 //Answer the following
 
-/*
-1. Boolean
-2. String
-3. Objects
-4. Array
-5. Objects
+// 1. Boolean
+// 2. String
+// 3. Objects
+// 4. Array
+// 5. Objects
+
+
+
+//Take it Easy 
+
+//1. 
+
+const rainbow = ['red', 'orange', 'yellow', 'green', 'purple', 'blue'];
+
+//2. 
+
+rainbow[5];
+
+//3. 
+
+const Alex = {
+	favoriteFood: 'Seafood',
+	hobby: 'Skiing',
+	currentCity: 'Issaquah',
+	favoriteDataType: 'Objects'
+};
+
+//4. 
+
+Alex.favoriteDataType;
+
 
