dba0e08257644970611b7c82b8b6c2db195dc087
ortizk <ortizk@live.com>
2018-03-26 17:12:10 -0700

    4 commit - accessed limbo

--- answers.js
+++ answers.js
@@ -40,3 +40,28 @@ CRAZY OBJECT!
 4. console.log(crazyObject.larry.nicknames[1]);
 
 5. console.log(crazyObject.larry);
+
+OBJECT-CEPTION
+
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
+
+
