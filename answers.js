3b1b357442f32c0c4958fe813bbb3a5520091f38
simvirk5 <simvirk5@gmail.com>
2018-03-26 17:03:51 -0700

    third commit

--- answers.js
+++ answers.js
@@ -1,21 +1,43 @@
-const lightSwitch = {
-	on: 'true',
-	}
-i.e. Boolean
-const userInfo = {
-	name: "x",
-	emailaddress: "johndoe@gmail.com"
+// const lightSwitch = {
+// 	on: 'true',
+// 	}
+// i.e. Boolean
+// const userInfo = {
+// 	name: "x",
+// 	emailaddress: "johndoe@gmail.com"
+// }
+// i.e. Boolean, string, object, array
+
+// const spaceship = [
+// 	{hull: "true",
+// 	laser: "false",
+// 	tractorbeam: "low",
+// 	warpdrive: "none",
+// } ]
+// i.e. string, array, object
+
+// const namesOfStudents = {
+// 	[name: '', location: '', favoriteShow: '']}
+// i.e. strings, arrays within an object, use (sort) function
+
+
+
+const rainbow = ["indigo", "red", "orange", "yellow", "blue", "green", "violent"]
+rainbow[4];
+
+const introduce = {
+	name: "Simmy",
+	food: "tacos",
+	hobby: "dance",
+	residence: "bothell",
+	datatype: "objects",
 }
-i.e. Boolean, string, object, array
+console.log(introduce);
+introduce.hobby
 
-const spaceship = [
-	{hull: "true",
-	laser: "false",
-	tractorbeam: "low",
-	warpdrive: "none",
-} ]
-i.e. string, array, object
+console.log(crazyObject.tacos[1].salsa[5]);
+console.log(crazyObject.larry.quotes[0]);
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+console.log(crazyObject.larry.nicknames[1]);
+console.log(crazyObject.larry.characters);
 
-const namesOfStudents = {
-	[name: '', location: '', favoriteShow: '']}
-i.e. strings, arrays within an object, use (sort) function
